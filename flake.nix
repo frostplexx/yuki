6a33980e8ff3d19892e47def7257511b842ce4b2
daniel <daniel.inama02@gmail.com>
2024-11-05 20:23:50 +0100

    feat: add module

--- flake.nix
+++ flake.nix
@@ -13,6 +13,81 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, rust-overlay, crane, flake-utils, ... }:
+    let
+      # Define the module that can be imported by NixOS and nix-darwin
+      yukiModule = { config, lib, pkgs, ... }: with lib; {
+        options.programs.yuki = {
+          enable = mkEnableOption "yuki package manager";
+          settings = mkOption {
+            type = types.submodule {
+              options = {
+                darwin_packages_path = mkOption {
+                  type = types.str;
+                  default = "~/dotfiles/hosts/darwin/apps.nix";
+                  description = "Path to Darwin packages configuration";
+                };
+                linux_packages_path = mkOption {
+                  type = types.str;
+                  default = "~/dotfiles/hosts/nixos/apps.nix";
+                  description = "Path to Linux packages configuration";
+                };
+                homebrew_packages_path = mkOption {
+                  type = types.str;
+                  default = "~/dotfiles/hosts/darwin/apps.nix";
+                  description = "Path to Homebrew packages configuration";
+                };
+                auto_commit = mkOption {
+                  type = types.bool;
+                  default = true;
+                  description = "Automatically commit changes";
+                };
+                auto_push = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = "Automatically push changes";
+                };
+                install_message = mkOption {
+                  type = types.str;
+                  default = "installed <package>";
+                  description = "Git commit message for package installation";
+                };
+                uninstall_message = mkOption {
+                  type = types.str;
+                  default = "removed <package>";
+                  description = "Git commit message for package removal";
+                };
+                install_command = mkOption {
+                  type = types.str;
+                  default = "make";
+                  description = "Command to run after package installation";
+                };
+                uninstall_command = mkOption {
+                  type = types.str;
+                  default = "make";
+                  description = "Command to run after package removal";
+                };
+                update_command = mkOption {
+                  type = types.str;
+                  default = "make update";
+                  description = "Command to run for updating packages";
+                };
+              };
+            };
+            default = {};
+          };
+        };
+
+        config = mkIf config.programs.yuki.enable {
+          environment.systemPackages = let
+            system = pkgs.system;
+          in [
+            (self.packages.${system}.default.override {
+              yukiConfig = config.programs.yuki.settings;
+            })
+          ];
+        };
+      };
+    in
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -20,8 +95,8 @@
           inherit system overlays;
         };
         
-        # Yuki configuration
-        yukiConfig = {
+        # Yuki configuration defaults
+        yukiConfigDefault = {
           linux_packages_path = "~/dotfiles/hosts/nixos/apps.nix";
           darwin_packages_path = "~/dotfiles/hosts/darwin/apps.nix";
           homebrew_packages_path = "~/dotfiles/hosts/darwin/apps.nix";
@@ -40,11 +115,8 @@
         
         # Common dependencies for building and development
         commonDeps = with pkgs; [
-          # Build dependencies
           pkg-config
           openssl
-          
-          # Runtime dependencies
           git
           nix
         ] ++ lib.optionals stdenv.isDarwin [
@@ -52,13 +124,13 @@
           darwin.apple_sdk.frameworks.SystemConfiguration
         ];
         
-        # Project source with only the necessary files
         src = craneLib.cleanCargoSource ./.;
         
-        # Common arguments for building
-        commonArgs = {
+        # Function to create build args with specific config
+        mkBuildArgs = yukiConfig: {
           inherit src;
           buildInputs = commonDeps;
+          
           # Pass configuration as environment variables
           YUKI_LINUX_PACKAGES_PATH = yukiConfig.linux_packages_path;
           YUKI_DARWIN_PACKAGES_PATH = yukiConfig.darwin_packages_path;
@@ -72,68 +144,49 @@
           YUKI_UPDATE_COMMAND = yukiConfig.update_command;
         };
         
-        # Build dependencies separately to improve caching
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        # Build dependencies with default config
+        cargoArtifacts = craneLib.buildDepsOnly (mkBuildArgs yukiConfigDefault);
         
-        # The package itself with configuration
-        yuki = craneLib.buildPackage (commonArgs // {
+        # Function to create the package with specific config
+        mkYuki = yukiConfig: craneLib.buildPackage ((mkBuildArgs yukiConfig) // {
           inherit cargoArtifacts;
         });
         
-        # Wrapper script to ensure environment variables are set
-        yukiWrapper = pkgs.writeShellScriptBin "yuki" ''
-          export YUKI_LINUX_PACKAGES_PATH="${yukiConfig.linux_packages_path}"
-          export YUKI_DARWIN_PACKAGES_PATH="${yukiConfig.darwin_packages_path}"
-          export YUKI_HOMEBREW_PACKAGES_PATH="${yukiConfig.homebrew_packages_path}"
-          export YUKI_AUTO_COMMIT="${toString yukiConfig.auto_commit}"
-          export YUKI_AUTO_PUSH="${toString yukiConfig.auto_push}"
-          export YUKI_UNINSTALL_MESSAGE="${yukiConfig.uninstall_message}"
-          export YUKI_INSTALL_MESSAGE="${yukiConfig.install_message}"
-          export YUKI_INSTALL_COMMAND="${yukiConfig.install_command}"
-          export YUKI_UNINSTALL_COMMAND="${yukiConfig.uninstall_command}"
-          export YUKI_UPDATE_COMMAND="${yukiConfig.update_command}"
-          exec ${yuki}/bin/yuki "$@"
-        '';
+        # Default package with ability to override config
+        defaultYuki = mkYuki yukiConfigDefault;
       in {
         checks = {
-          inherit yuki;
-          clippy = craneLib.cargoClippy (commonArgs // {
+          inherit defaultYuki;
+          clippy = craneLib.cargoClippy ((mkBuildArgs yukiConfigDefault) // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           });
-          test = craneLib.cargoTest (commonArgs // {
+          test = craneLib.cargoTest ((mkBuildArgs yukiConfigDefault) // {
             inherit cargoArtifacts;
           });
         };
+        
         packages = {
-          default = yukiWrapper;
-          unwrapped = yuki;
+          default = defaultYuki;
         };
+        
         apps.default = flake-utils.lib.mkApp {
-          drv = yukiWrapper;
+          drv = defaultYuki;
         };
+        
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ yuki ];
+          inputsFrom = [ defaultYuki ];
           buildInputs = with pkgs; [
             rustToolchain
             rust-analyzer
             cargo-watch
             cargo-edit
           ] ++ commonDeps;
-          # Include configuration in development shell
-          shellHook = ''
-            export YUKI_LINUX_PACKAGES_PATH="${yukiConfig.linux_packages_path}"
-            export YUKI_DARWIN_PACKAGES_PATH="${yukiConfig.darwin_packages_path}"
-            export YUKI_HOMEBREW_PACKAGES_PATH="${yukiConfig.homebrew_packages_path}"
-            export YUKI_AUTO_COMMIT="${toString yukiConfig.auto_commit}"
-            export YUKI_AUTO_PUSH="${toString yukiConfig.auto_push}"
-            export YUKI_UNINSTALL_MESSAGE="${yukiConfig.uninstall_message}"
-            export YUKI_INSTALL_MESSAGE="${yukiConfig.install_message}"
-            export YUKI_INSTALL_COMMAND="${yukiConfig.install_command}"
-            export YUKI_UNINSTALL_COMMAND="${yukiConfig.uninstall_command}"
-            export YUKI_UPDATE_COMMAND="${yukiConfig.update_command}"
-          '';
         };
       }
-    );
+    ) // {
+      # Export the module for use in NixOS and nix-darwin
+      nixosModules.default = yukiModule;
+      darwinModules.default = yukiModule;
+    };
 }
